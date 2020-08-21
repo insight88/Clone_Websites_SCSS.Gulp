97b28ed3fd8efdf265b43f13d4677c10b91a094c
insight88 <kpkim1988@gmail.com>
2020-08-21 18:27:13 +0900

    http://10x19.co/

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,7 @@ sass.compiler = require("node-sass");
 const routes = {
     css: {
         watch: "src/scss/*",
-        src: "src/scss/styles2.scss",
+        src: "src/scss/styles3.scss",
         dest: "dist/css",
     },
 };
