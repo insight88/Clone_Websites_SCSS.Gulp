9a6a32ecbe53c72d188fe5373df9bb8e0a2d180d
insight88 <64120339+insight88@users.noreply.github.com>
2020-08-23 16:55:02 +0900

    http://www.z-o-o.fr/

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,7 @@ sass.compiler = require("node-sass");
 const routes = {
     css: {
         watch: "src/scss/*",
-        src: "src/scss/styles3.scss",
+        src: "src/scss/styles4.scss",
         dest: "dist/css",
     },
 };
