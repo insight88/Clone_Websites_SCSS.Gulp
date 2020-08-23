c175ef4a493496744059ca6f7abaec9fd388b0a1
insight88 <64120339+insight88@users.noreply.github.com>
2020-08-23 18:55:26 +0900

    https://canalstreet.market/

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,7 @@ sass.compiler = require("node-sass");
 const routes = {
     css: {
         watch: "src/scss/*",
-        src: "src/scss/styles9.scss",
+        src: "src/scss/styles10.scss",
         dest: "dist/css",
     },
 };
