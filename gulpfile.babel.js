6f24765b540739087e8e843b2e780b937941feb5
insight88 <64120339+insight88@users.noreply.github.com>
2020-08-23 18:26:21 +0900

    https://beige.de/

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,7 +9,7 @@ sass.compiler = require("node-sass");
 const routes = {
     css: {
         watch: "src/scss/*",
-        src: "src/scss/styles7.scss",
+        src: "src/scss/styles8.scss",
         dest: "dist/css",
     },
 };
